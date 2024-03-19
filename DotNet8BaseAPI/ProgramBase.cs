b4395de1cfcd0e11bf20176422bcce0723e7f164
Abhishek Kumar <abhik1109@outlook.com>
2024-03-20 00:04:44 +0530

    Add project files.

---
+++ DotNet8BaseAPI/ProgramBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace DotNet8BaseAPI
+{
+    public static class ProgramBase
+    {
+        public static WebApplicationBuilder RegisterBaseServices(this WebApplicationBuilder builder)
+        {
+
+            return builder;
+        }
+
+        public static WebApplication ConfigureBaseHttpPipeline(this WebApplication app)
+        {
+
+            return app;
+        }
+    }
+}
